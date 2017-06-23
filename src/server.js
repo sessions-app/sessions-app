2d9549f2663267ae441210721cedae0580a94f2a
Matt Pace <pace4@purdue.edu>
2017-06-22 21:49:06 -0700

    Add sequelize structure and test

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const sequelize = require('./models').sequelize;
 
 const app = express();
+
+app.get('/', (req, res) => {
+  sequelize.authenticate().then(() => {
+    res.send('Was able to connect to database');
+  }).catch(() => {
+    res.send('Was unable to connect to database');
+  });
+});
 
 app.listen(8080);
