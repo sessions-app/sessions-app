330cfcd40568293ca80fa1d8dcc00fbcb0ddfe66
Matthew Fischer <matthew.gfischer@gmail.com>
2017-07-07 20:03:35 -0700

    Installs bootstrap and sass-loader/dependencies
    Sets up stylepath and loads bundled stylesheet into the main layout
    
    Adds bootstrap styling to react component
    
    Changes extract-text to be a regular dependency
    
    Adds sass, style, and css loaders to be dependencies
    
    Adds node-sass as dependency
    
    Fixes linter errors
    
    Fixes syntax error
    
    Adds newline at end of file

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
-const Hello = props => <div> Hello {props.name} </div>;
+const Hello = props => <div className='well well-lg'> Hello {props.name} </div>;
 
 Hello.propTypes = {
   name: PropTypes.string.isRequired,
